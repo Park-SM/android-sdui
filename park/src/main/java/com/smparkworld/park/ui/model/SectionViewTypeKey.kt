d1b83a8ec823652597f36d03728e9f42799cfa57
parksm <sangmin.park@myrealtrip.com>
2022-12-14 21:38:32 +0900

    Added ViewType sample codes

--- park/src/main/java/com/smparkworld/park/ui/model/SectionViewTypeKey.kt
+++ park/src/main/java/com/smparkworld/park/ui/model/SectionViewTypeKey.kt
@@ -5,4 +5,6 @@ object SectionViewTypeKey {
     const val DEFAULT_VIEW_TYPE = "DEFAULT_VIEW_TYPE"
 
     const val PRODUCT_ONE_COLUMN = "PRODUCT_ONE_COLUMN"
+
+    const val PRODUCT_ONE_COLUMN_2 = "PRODUCT_ONE_COLUMN_2"
 }
