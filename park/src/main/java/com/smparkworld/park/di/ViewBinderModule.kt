94fea45f42392d19cac08bf1e0e61fbff0bd8676
SangMin Park <park97.sm@gmail.com>
2022-12-17 18:18:28 +0900

    Move ViewBinders package

--- park/src/main/java/com/smparkworld/park/di/ViewBinderModule.kt
+++ park/src/main/java/com/smparkworld/park/di/ViewBinderModule.kt
@@ -5,8 +5,8 @@ import com.smparkworld.park.di.annotation.SectionViewBinders
 import com.smparkworld.park.domain.dto.SectionDTO
 import com.smparkworld.park.ui.park.model.SectionViewBinder
 import com.smparkworld.park.ui.park.model.SectionViewTypeKey
-import com.smparkworld.park.ui.viewbinder.ProductOneColumn2ViewBinder
-import com.smparkworld.park.ui.viewbinder.ProductOneColumnViewBinder
+import com.smparkworld.park.ui.park.model.viewbinder.ProductOneColumn2ViewBinder
+import com.smparkworld.park.ui.park.model.viewbinder.ProductOneColumnViewBinder
 import dagger.Binds
 import dagger.Module
 import dagger.hilt.InstallIn
